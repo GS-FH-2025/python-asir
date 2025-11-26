7ca6d230ecde00d85cb5c2936a89d0c3056c1efa
lemonada02 <jrlimon02@gmail.com>
2025-11-26 19:08:35 +0100

    Introducción

---
+++ variables.py
@@ -0,0 +1,66 @@
+# # STRING(str) --> ?,$,4,p
+# variable = "Hola me llamo juan"
+# print(type(variable))
+# print(variable)
+
+# # INTEGER(int) --> 4, 173, 0
+# numero = 5
+# print(type(numero))
+# print(numero)
+
+# # FLOAT(float) --> 4.0, 173.5, 0.9
+# barco = 13.56
+# print(type(barco))
+# print(barco)
+
+# # BOOLEAN(bool) --> True, False
+# booleano= True
+# print(type(booleano))
+# print(booleano)
+
+
+
+
+
+# TIPOS DE VARIABLES -- INTEGER/FLOAT
+# numero1 = int(input('Dame un número: '))   #str --> int
+# numero2 = float(input('Dame otro número: ')) #str --> int
+
+# cuenta1 = numero1 / numero2  # division normal
+# cuenta2 = numero1 // numero2  # division entera (solo cociente)
+# cuenta3 = numero1 % numero2  # division entera (solo resto)
+
+# cuenta = numero1 + numero2
+# print(int(cuenta))
+
+
+# TIPOS DE VARIABLES -- STRING
+# caracter1= input('Dame una letra: ')
+# caracter2= 'becedario'
+# caracter3= '2025'
+# print(caracter1+caracter2+caracter3)
+
+# dato= True
+# print(type(dato))
+# print(dato)
+
+# dato= str(True)
+# print(type(dato))
+# print(dato)
+
+
+# TIPOS DE VARIABLES -- BOOLEAN
+
+# variable1 = True
+# variable2 = False  # 0, 0.0, ''(vacío sin espacios)
+
+# print(bool(numero1))
+# print(bool(caracter1))
+
+# print( 5 <= 7 )
+
+
+
+
+
+
